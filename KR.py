2e1df497575e2b30a7de9ce3528559f95fedc95c
VicDoom <55551616+VicDoom@users.noreply.github.com>
2021-05-18 22:32:05 +0300

    Update KR.py

--- KR.py
+++ KR.py
@@ -1,5 +1,5 @@
 from str_search import StrSearch
-
+# алг Карпа-Рабена
 class KR(StrSearch):
     def search(self, this):
         print("\nKR...")
@@ -11,12 +11,15 @@ class KR(StrSearch):
         self.text_hash = []
         result = []
         result_compare = 0
-
+        
+        # вычисляем хэш для паттерна
         self.pattern_hash = self.hash(0, self.pattern)
 
+        # вычисляем хэш для текста последовательно, каждый следующий хэш вычисляется из предыдущего
         for i in range(self.n-self.m+1):
             self.text_hash.append(self.hash(i, self.text))
 
+        # определяем вхождения - если хэш паттерна равен соответствующего хэша текста в данной позиции, то определено равенство подстроки и паттерна
         for i in range(len(self.text_hash)):
             if self.text_hash[i] == self.pattern_hash:
                 result.append(i)
@@ -28,6 +31,7 @@ class KR(StrSearch):
 
         return result, result_compare
 
+    # хэш считается по сложной формуле с Википедии https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%A0%D0%B0%D0%B1%D0%B8%D0%BD%D0%B0_%E2%80%94_%D0%9A%D0%B0%D1%80%D0%BF%D0%B0
     def hash(self, i, text):
         result = 0
         if i == 0:
@@ -42,6 +46,7 @@ class KR(StrSearch):
             result = self.module_sum(self.module_multiple(tmp_result, self.x), ord(text[i+self.m-1]))
         return result % self.big_prime_num
 
+    # эти функции определяют возведение в степень, сложение, умножение и деление по модулю (чтобы не было слишком больших чисел)
     def module_power(self, a, n):
         x = 1
         for i in range(n):
